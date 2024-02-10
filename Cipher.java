bfa01f784d6a54b2f3fc7ada62fc241668b0f268
Umut Övünç Şenol <145434910+umutovuncsenol@users.noreply.github.com>
2024-02-10 16:26:20 +0300

    Update Cipher.java
    The missing parts in the Cipher file has been fixed.

--- Cipher.java
+++ Cipher.java
@@ -15,7 +15,9 @@ public class Cipher
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
-
+            char currentChar = inputString.charAt(i);
+            char encryptedChar = replaceChar(currentChar, true);
+            outputString += encryptedChar;
         }
 
         return outputString;
@@ -26,7 +28,12 @@ public class Cipher
         // output string will be collected in this variable, one char at a time
         String outputString = "";
         
-        replaceChar('a',true);
+        for (int i = 0; i < inputString.length(); i++) 
+        {
+            char currentChar = inputString.charAt(i);
+            char decryptedChar = replaceChar(currentChar, false);
+            outputString += decryptedChar;
+        }
         
         return outputString;
     }
@@ -42,7 +49,7 @@ public class Cipher
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
                 if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
-
+                    return CIPHER_ALPHABET.charAt(i);
                 }
             }
         }
